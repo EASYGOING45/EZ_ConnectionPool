25076357e19cd23ecf0cfbbfa6bbe1689f7e46b4
EASYGOING45 <1505674928@qq.com>
2023-03-19 16:12:38 +0800

    使用生产者-消费者模型实现数据库连接池

--- src/EZ_ConnectionPool.cpp
+++ src/EZ_ConnectionPool.cpp
@@ -102,3 +102,90 @@ ConnectionPool::ConnectionPool()
     thread scanner(std::bind(&ConnectionPool::scannerConnectionTask, this));
     scanner.detach();
 }
+
+// 运行在独立的线程中，专门负责生产新连接
+void ConnectionPool::produceConnectionTask()
+{
+    for (;;)
+    {
+        unique_lock<mutex> lock(_queueMutex); // Lock
+        while (!_connectionQue.empty())
+        {
+            cv.wait(lock); // 队列不空，此时生产线程进入等待状态
+        }
+
+        // 连接数量没有到达上限，继续创建新的连接
+        if (_connectionCnt < _maxSize)
+        {
+            Connection *p = new Connection();
+            p->connect(_ip, _port, _username, _password, _dbname); // 一条新的连接
+            p->refreshAliveTime();                                 // 刷新开始空闲的起始时间
+            _connectionQue.push(p);
+            _connectionCnt++;
+        }
+
+        // 通知消费者线程，可以进行消费了
+        cv.notify_all(); // notify_all
+    }
+}
+
+shared_ptr<Connection> ConnectionPool::getConnecction()
+{
+    unique_lock<mutex> lock(_queueMutex);
+    while (_connectionQue.empty())
+    {
+        // sleep
+        if (cv_status::timeout == cv.wait_for(lock, chrono::milliseconds(_connectionTimeout)))
+        {
+            if (_connectionQue.empty())
+            {
+                LOG("获取空闲连接超时......获取失败!");
+                return nullptr;
+            }
+        }
+    }
+
+    /**
+     * shared_ptr智能指针析构时，会把connection资源直接delete掉，相当于调用connection的析构函数，connection就被close掉了。
+     * 这里需要自定义shared_ptr的释放资源的方式，把connection直接归还到queue当中，而不是delete掉。
+     */
+    shared_ptr<Connection> sp(_connectionQue.front(),
+                              [&](Connection *pcon)
+                              {
+                                  // 此处在服务器应用线程中调用，所以一定要考虑队列的线程安全操作
+                                  unique_lock<mutex> lock(_queueMutex);
+                                  pcon->refreshAliveTime(); // 刷新一下开始空闲的起始时间
+                                  _connectionQue.push(pcon);
+                              });
+    _connectionQue.pop(); // 从队列中取出一个连接
+    cv.notify_all();      // 通知生产者线程，可以进行生产了
+
+    return sp; // 返回一个shared_ptr
+}
+
+// 运行在独立的线程中，专门负责扫描超时的空闲连接
+void ConnectionPool::scannerConnectionTask()
+{
+    for (;;)
+    {
+        // 通过sleep来模拟定时效果
+        this_thread::sleep_for(chrono::seconds(_maxIdleTime));
+
+        // 扫描整个队列，释放多余的连接
+        unique_lock<mutex> lock(_queueMutex);
+        while (_connectionCnt > _initSize)
+        {
+            Connection *p = _connectionQue.front();
+            if (p->getAliveTime() >= {_maxIdleTime * 1000})
+            {
+                _connectionQue.pop();
+                _connectionCnt--;
+                delete p; // 调用~Connection()释放连接
+            }
+            else
+            {
+                break; // 队头的连接没有超过_maxIdleTime，其他连接肯定也没有
+            }
+        }
+    }
+}
